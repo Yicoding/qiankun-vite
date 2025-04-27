97d9f495889a8b84dd9d753e2bcf497e264440f3
kaiyu.kong <kaiyu.kong@ximalaya.com>
2025-04-27 17:24:54 +0800

    feat: dumi配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,11 +4,12 @@ const logo =
   'https://imagev2.xmcdn.com/storages/f8d2-audiofreehighqps/81/43/GMCoOSYIO18uAAAvaAIdYbXD.png';
 
 const publicPath =
-  process.env.NODE_ENV === 'production' ? `/cli/micro-scripts/` : '/';
+  process.env.NODE_ENV === 'production' ? `/qiankun-vite/master/` : '/';
 
 export default defineConfig({
+  outputPath: 'site',
   themeConfig: {
-    name: 'qiankun-template',
+    name: 'qiankun-vite',
     logo,
   },
   history: {
